18d387aa3ad4101a746a0e7a17617702d32ce795
npdangkhoa <npdangkhoa@gmail.com>
2021-10-13 14:57:38 +0700

    rename

--- mssc-brewery/src/test/java/guru/springframwork/mssc/brewery/ApplicationTests.java
+++ mssc-brewery/src/test/java/guru/springframwork/mssc/brewery/ApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframwork.mssc.breway;
+package guru.springframwork.mssc.brewery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
