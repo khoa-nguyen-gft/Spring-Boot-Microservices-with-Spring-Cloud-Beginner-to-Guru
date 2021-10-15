a8cb9a32de1b2e59eea9bd5850459256faa77e78
npdangkhoa <npdangkhoa@gmail.com>
2021-10-15 14:10:42 +0700

    add new services

---
+++ mssc-beer-service/src/main/java/guru/springframwork/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package guru.springframwork.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
