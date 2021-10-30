7a86cef7a77542a45e1209607bbb8f8ec059dd8e
Nguyen, Khoa <npdangkhoa@gmail.com>
2021-10-30 17:41:59 +0700

    init inventory service and order service

---
+++ mssc-beer-service/src/test/java/guru/springframwork/msscbeerservice/web/services/BeerServiceImplTest.java
@@ -0,0 +1,20 @@
+package guru.springframwork.msscbeerservice.web.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerServiceImplTest {
+
+    @Test
+    void getBeerById() {
+    }
+
+    @Test
+    void saveBeer() {
+    }
+
+    @Test
+    void updateBeerById() {
+    }
+}
